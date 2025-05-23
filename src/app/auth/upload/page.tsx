f13a7eea02ded36d474b9f96642ff04c9a70043b
Jeronimo <jeronimo@Jeronimos-MacBook-Pro.local>
2025-05-23 13:40:08 -0500

    Updated with all

---
+++ src/app/auth/upload/page.tsx
@@ -0,0 +1,7 @@
+export default function UploadPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Hi</h1>
+    </div>
+  );
+}
